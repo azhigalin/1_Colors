cccbda03f89ab3c4ce8ba8c11044d9160c407b44
Aleksandr <azhigalin20@gmail.com>
2022-12-12 21:29:26 +0300

    Commit 2
    Усовершенствовал код путем создания функции генерации случайного цвета.

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-const colors = [
-  "#8B8940",
-  "#B2EC5D",
-  "#f34723",
-  "#2b2517",
-  "#ddadaf",
-  "#003841",
-  "#faebd7",
-  "#1dacd6",
-  "#693623",
-  "#ace1af",
-];
-let index = 0;
-
 document.querySelector("button").addEventListener("click", () => {
-  document.body.style.backgroundColor = colors[index++];
-  if (index === colors.length) index = 0;
+  document.body.style.backgroundColor = generateColor();
 });
+
+let generateColor = function () {
+  return "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+};
